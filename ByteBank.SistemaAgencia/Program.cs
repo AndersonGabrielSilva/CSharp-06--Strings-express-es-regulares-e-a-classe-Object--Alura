3eccc8437315120016593c093ee77cc2e1b0104d
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2020-03-11 14:08:46 -0300

    Manipulando Strings/SubStrings

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -13,17 +13,28 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
         {
 
-            DateTime dataFimPagamento = new DateTime(2020, 3, 15);
-            DateTime dataCorrente = DateTime.Now;
+            // paginas? argumentos
+            // 0123
 
-            //TimeSpan diferenca = dataFimPagamento - dataCorrente;
-            TimeSpan diferenca = TimeSpan.FromMinutes(60);
+            /*No CSharp a classe "String", ela é imutavel, ou seja não é possivel fazer alguma alteração.
+             *Lembra os vetores no C aonde cada string é uma cadeia de caracteres aonde cada posição é um caractere 
+             *que por fim acaba criando uma frase.
+             * O Metodo abaixo não é permitido, justamente por conta desta caracteristica do c#*/
+            //url = url.Substring(7);
 
-            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            string url = "paginas?argumentos";
+            Console.WriteLine(url);
 
-            Console.WriteLine(dataCorrente);
-            Console.WriteLine(dataFimPagamento);
-            Console.WriteLine(mensagem);
+            /*Ao fazer a operação abaixo,o que na verdade acontece é que por baixo dos panos o C# cria uma string Temporaria aonde 
+             ela recebe os caracteres da string url somados com os caracteres da string "Anderson". 
+             *E depois de receber os valores,  compilador simplesmente altera a referencia (O endereço na memoria)*/
+            url += " Anderson";
+            Console.WriteLine(url);
+
+
+            string url2;
+            url2=url.Substring(7);            
+            Console.WriteLine(url2);
 
             Console.ReadLine();
 
